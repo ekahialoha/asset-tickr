bc0d37938e280fa79a358399de566486cf94acb6
Christian Kelsom-Martin <christian.kelsommartin@gmail.com>
2020-08-28 20:13:43 -0700

    Add dotenv, ws, express-we

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const ws = require('ws');
 
 const PORT = process.env.PORT || 3001;
+
 const app = express();
+require('express-ws')(app);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/client/build')));
